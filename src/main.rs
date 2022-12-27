c2cb42ac4d340d4fa762d823c4c095e7d3fc2263
MyselfLeo <myselfleo@outlook.fr>
2022-12-27 15:28:32 +0100

    /

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod big_num;
 use big_num::BigNum;
 
 fn main() {
-    let b1 = BigNum::from_string("-10").unwrap();
-    let b2 = BigNum::from_string("10").unwrap();
+    let b1 = BigNum::from_string("1242005").unwrap();
+    let b2 = BigNum::from_string("0.23352").unwrap();
 
     println!("{b1} + {b2} = {}", BigNum::add(&b1, &b2));
     println!("{b1} - {b2} = {}", BigNum::sub(&b1, &b2));
