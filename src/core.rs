166d86b4cf21f6b3a9684347bfb0eedfc5a1915f
MyselfLeo <myselfleo@outlook.fr>
2023-01-07 15:22:13 +0100

    added addition

--- src/core.rs
+++ src/core.rs
@@ -1,30 +1,65 @@
 
-/// Clean useless zeroes of the integer
-fn clean_uint(u: &mut Vec<u8>) {
-    while let Some(0) = u.last() {u.pop();}
+/// Clean useless zeroes of the big int
+fn clean_ubint(ubint: &mut Vec<u8>) {
+    while let Some(0) = ubint.last() {ubint.pop();}
+}
+
+
+
+
+/// Add 2 unsigned bit ints u and v
+/// (represented by vecs of u8, from least to most significant digit)
+pub fn ub_add(u: Vec<u8>, v: Vec<u8>) -> Vec<u8> {
+    // the algorithm requires that u.len() >= v.len()
+    if u.len() < v.len() {return ub_add(v, u)}
+
+    // various optimization
+    if u == vec![0] {return v;}
+    if v == vec![0] {return u;}
+
+    let m = u.len();
+    let n = v.len();
+
+    let mut w = vec![0; m+1];
+
+    // addition by iterating over digits of u
+    let mut k = 0;
+    for i in 0..m {
+        let vi = if i < n {v[i]} else {0};
+        let t = u[i] + vi + k;
+
+        w[i] = t % 10;
+        k = t / 10;
+    }
+    w[m] = k; // final carry
+
+    clean_ubint(&mut w);
+    w
 }
 
 
 
 
 
-/// Multiply 2 unsigned integers
+
+/// Multiply 2 unsigned big ints u and v
 /// (represented by vecs of u8, from least to most significant digit)
 /// Base on the multiplication algorithm in the Art of Computer Programming
-pub fn u_mul(u: Vec<u8>, v: Vec<u8>) -> Vec<u8> {
+pub fn ub_mul(u: Vec<u8>, v: Vec<u8>) -> Vec<u8> {
     // the algorithm requires that u.len() >= v.len()
-    if u.len() < v.len() {return u_mul(v, u)}
+    if u.len() < v.len() {return ub_mul(v, u)}
 
 
     // various optimisation
     if v == vec![1] {return u;}
+    if u == vec![1] {return v;}
     if u == vec![0] || v == vec![0] {return vec![0];}
 
     
     let m = u.len();
     let n = v.len();
 
-    let mut w = vec![0; m+n];
+    let mut w = vec![0; m+n];   // final big uint
 
     // multiplication by iterating over each digit of v
     for j in 0..n {
@@ -39,9 +74,8 @@ pub fn u_mul(u: Vec<u8>, v: Vec<u8>) -> Vec<u8> {
             w[i+j] = t % 10;
             k = t / 10;
         }
-        w[j+m] = k;
+        w[j+m] = k; // final carry
     }
-
-    clean_uint(&mut w);
+    clean_ubint(&mut w);
     w
 }
