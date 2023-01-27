0327dafb258efda382482d40f66166deb2bc98ce
MyselfLeo <myselfleo@outlook.fr>
2023-01-27 08:44:55 +0100

    /.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! # Sloth-num
 //! 
-//! The big number library created for and used by the programming language (Sloth)[https://crates.io/crates/slothlang]
+//! The big number library created for and used by the programming language [Sloth](https://crates.io/crates/slothlang).
 
 
 mod core;
-pub mod big_num;
+mod big_num;
 pub use big_num::BigNum;
