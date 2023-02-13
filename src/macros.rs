b64843e9f41b9de80b87fe52262961d99b5f7e75
MyselfLeo <myselfleo@outlook.fr>
2023-02-13 23:55:54 +0100

    /

--- src/macros.rs
+++ src/macros.rs
@@ -4,6 +4,7 @@
 /// Assert that the given condition is true.  
 /// If not, return the given error msg as an error.  
 /// Of course, this macro can only be used in a function with the return type `Result<_, String>`
+#[doc(hidden)]
 #[macro_export]
 macro_rules! assert_err {
     ($condition:expr, $($arg:tt)+) => {
