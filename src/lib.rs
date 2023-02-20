b67c080e5d40d02350f0270d06987b12ddfde7fb
MyselfLeo <myselfleo@outlook.fr>
2023-02-20 08:06:10 +0100

    doc style change

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! 
 //! The big number library created for and used by the programming language [Sloth](https://crates.io/crates/slothlang).
 //! 
-//! The main struct is **[BigNum]**. In most of the cases, you won't need to use [core].
+//! The main struct is [BigNum]. In most of the cases, you won't need to use [core].
 
 mod macros;
 pub mod core;
